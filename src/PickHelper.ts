b3bde3071508578bb45545e24187e57053206ae3
SimplyNate <horiuchi.nathan@gmail.com>
2023-06-09 15:29:10 -0700

    Highlight picked object

--- src/PickHelper.ts
+++ src/PickHelper.ts
@@ -3,21 +3,29 @@ import * as THREE from 'three';
 export default class {
     private raycaster: THREE.Raycaster;
     public pickedObject: null | THREE.Object3D;
+    public cache: any;
     constructor() {
         this.raycaster = new THREE.Raycaster();
         this.pickedObject = null;
+        this.cache = null;
     }
 
     pick(position: THREE.Vector2, scene: THREE.Scene, camera: THREE.Camera) {
         if (this.pickedObject) {
+            // @ts-ignore
+            this.pickedObject.material.color = this.cache;
             this.pickedObject.translateX(0);
             this.pickedObject = null;
+            this.cache = null;
         }
         this.raycaster.setFromCamera(position, camera);
         const intersectedObjects = this.raycaster.intersectObjects(scene.children);
         if (intersectedObjects.length > 0) {
             this.pickedObject = intersectedObjects[0].object;
-            this.pickedObject.translateX(0);
+            // @ts-ignore
+            this.cache = this.pickedObject.material.color;
+            // @ts-ignore
+            this.pickedObject.material.color = new THREE.Color(0xffffff);
         }
     }
 }
