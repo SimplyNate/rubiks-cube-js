a180d69f9d309cc65d1cfafed4b9a1ff9a7a554b
SimplyNate <horiuchi.nathan@gmail.com>
2023-06-09 23:07:54 -0700

    Statically draw all stroked edges of cube

--- src/shapes.ts
+++ src/shapes.ts
@@ -18,14 +18,60 @@ export function createCube(scene: THREE.Scene, x: number, y: number, z: number) 
     scene.add(cubeMesh);
     
     const cylinder = new THREE.CylinderGeometry(0.03, 0.03, 1);
-    const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 'black'});
+    const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 'black' });
     const cylinderMesh = new THREE.Mesh(cylinder, cylinderMaterial);
-    cylinderMesh.position.set(x - 1 - 0.5, y - 1 + 0.5, z - 1);
+    cylinderMesh.position.set(x  - 0.5, y - 0.5, z);
     cylinderMesh.rotateX(THREE.MathUtils.degToRad(90));
     scene.add(cylinderMesh);
 
     const c2 = new THREE.Mesh(cylinder, cylinderMaterial);
-    c2.position.set(x - 1 + 0.5, y - 1 + 0.5, z - 1);
+    c2.position.set(x + 0.5, y - 0.5, z);
     c2.rotateX(THREE.MathUtils.degToRad(90));
     scene.add(c2);
+
+    const c3 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c3.position.set(x - 0.5, y + 0.5, z);
+    c3.rotateX(THREE.MathUtils.degToRad(90));
+    scene.add(c3);
+
+    const c4 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c4.position.set(x + 0.5, y + 0.5, z);
+    c4.rotateX(THREE.MathUtils.degToRad(90));
+    scene.add(c4);
+
+    const c5 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c5.position.set(x, y + 0.5, z + 0.5);
+    c5.rotateZ(THREE.MathUtils.degToRad(90));
+    scene.add(c5);
+
+    const c6 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c6.position.set(x, y + 0.5, z - 0.5);
+    c6.rotateZ(THREE.MathUtils.degToRad(90));
+    scene.add(c6);
+
+    const c7 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c7.position.set(x, y - 0.5, z + 0.5);
+    c7.rotateZ(THREE.MathUtils.degToRad(90));
+    scene.add(c7);
+
+    const c8 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c8.position.set(x, y - 0.5, z - 0.5);
+    c8.rotateZ(THREE.MathUtils.degToRad(90));
+    scene.add(c8);
+
+    const c9 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c9.position.set(x + 0.5, y, z + 0.5);
+    scene.add(c9);
+
+    const c10 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c10.position.set(x + 0.5, y, z - 0.5);
+    scene.add(c10);
+
+    const c11 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c11.position.set(x - 0.5, y, z + 0.5);
+    scene.add(c11);
+
+    const c12 = new THREE.Mesh(cylinder, cylinderMaterial);
+    c12.position.set(x - 0.5, y, z - 0.5);
+    scene.add(c12);
 }
