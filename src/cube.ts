737aec571f2a2de3276df6a434a270989957bceb
SimplyNate <horiuchi.nathan@gmail.com>
2023-09-08 18:21:01 -0700

    Implement l

--- src/cube.ts
+++ src/cube.ts
@@ -67,7 +67,32 @@ class Cube {
         this.cube.l[7] = lBeforeRotation[5];
         this.cube.l[8] = lBeforeRotation[2];
     }
-    l() {}
+    l() {
+        const firstCopy = [...this.cube.f];
+        this.cube.f[0] = this.cube.d[0];
+        this.cube.f[3] = this.cube.d[3];
+        this.cube.f[6] = this.cube.d[6];
+        this.cube.d[0] = this.cube.b[8];
+        this.cube.d[3] = this.cube.b[5];
+        this.cube.d[6] = this.cube.b[2];
+        this.cube.b[8] = this.cube.u[0];
+        this.cube.b[5] = this.cube.u[3];
+        this.cube.b[2] = this.cube.u[6];
+        this.cube.u[0] = firstCopy[0];
+        this.cube.u[3] = firstCopy[3];
+        this.cube.u[6] = firstCopy[6];
+
+        const lBeforeRotation = [...this.cube.l];
+        this.cube.l[0] = lBeforeRotation[2];
+        this.cube.l[1] = lBeforeRotation[5];
+        this.cube.l[2] = lBeforeRotation[8];
+        this.cube.l[3] = lBeforeRotation[2];
+        this.cube.l[4] = lBeforeRotation[4];
+        this.cube.l[5] = lBeforeRotation[7];
+        this.cube.l[6] = lBeforeRotation[0];
+        this.cube.l[7] = lBeforeRotation[3];
+        this.cube.l[8] = lBeforeRotation[6];
+    }
 
     R() {}
     r() {}
