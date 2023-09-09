b3d4f0acb902956cb4dbf46df2775f4eb1ce4e01
SimplyNate <horiuchi.nathan@gmail.com>
2023-09-09 13:01:24 -0700

    Add generalized clockwise and counter clockwise rotation functions

--- src/cube.ts
+++ src/cube.ts
@@ -41,6 +41,58 @@ export class Cube {
         };
     }
 
+    private clockwiseRotation(face: string) {
+        /* old algorithm
+        const beforeRotation = [...this.cube[face]];
+        this.cube[face][0] = beforeRotation[6];
+        this.cube[face][1] = beforeRotation[3];
+        this.cube[face][2] = beforeRotation[0];
+        this.cube[face][3] = beforeRotation[7];
+        this.cube[face][4] = beforeRotation[4];
+        this.cube[face][5] = beforeRotation[1];
+        this.cube[face][6] = beforeRotation[8];
+        this.cube[face][7] = beforeRotation[5];
+        this.cube[face][8] = beforeRotation[2];
+         */
+        this.cube[face] = [
+            this.cube[face][6],
+            this.cube[face][3],
+            this.cube[face][0],
+            this.cube[face][7],
+            this.cube[face][4],
+            this.cube[face][1],
+            this.cube[face][8],
+            this.cube[face][5],
+            this.cube[face][2],
+        ];
+    }
+
+    private counterClockwiseRotation(face: string) {
+        /*
+        const beforeRotation = [...this.cube[face]];
+        this.cube[face][0] = beforeRotation[2];
+        this.cube[face][1] = beforeRotation[5];
+        this.cube[face][2] = beforeRotation[8];
+        this.cube[face][3] = beforeRotation[2];
+        this.cube[face][4] = beforeRotation[4];
+        this.cube[face][5] = beforeRotation[7];
+        this.cube[face][6] = beforeRotation[0];
+        this.cube[face][7] = beforeRotation[3];
+        this.cube[face][8] = beforeRotation[6];
+         */
+        this.cube[face] = [
+            this.cube[face][2],
+            this.cube[face][5],
+            this.cube[face][8],
+            this.cube[face][2],
+            this.cube[face][4],
+            this.cube[face][7],
+            this.cube[face][0],
+            this.cube[face][3],
+            this.cube[face][6],
+        ];
+    }
+
     L() {
         const firstCopy = [...this.cube.f];
         this.cube.f[0] = this.cube.u[0];
@@ -56,16 +108,7 @@ export class Cube {
         this.cube.d[3] = firstCopy[3];
         this.cube.d[6] = firstCopy[6];
 
-        const lBeforeRotation = [...this.cube.l];
-        this.cube.l[0] = lBeforeRotation[6];
-        this.cube.l[1] = lBeforeRotation[3];
-        this.cube.l[2] = lBeforeRotation[0];
-        this.cube.l[3] = lBeforeRotation[7];
-        this.cube.l[4] = lBeforeRotation[4];
-        this.cube.l[5] = lBeforeRotation[1];
-        this.cube.l[6] = lBeforeRotation[8];
-        this.cube.l[7] = lBeforeRotation[5];
-        this.cube.l[8] = lBeforeRotation[2];
+        this.clockwiseRotation('l');
         return this;
     }
     l() {
@@ -83,16 +126,7 @@ export class Cube {
         this.cube.u[3] = firstCopy[3];
         this.cube.u[6] = firstCopy[6];
 
-        const lBeforeRotation = [...this.cube.l];
-        this.cube.l[0] = lBeforeRotation[2];
-        this.cube.l[1] = lBeforeRotation[5];
-        this.cube.l[2] = lBeforeRotation[8];
-        this.cube.l[3] = lBeforeRotation[2];
-        this.cube.l[4] = lBeforeRotation[4];
-        this.cube.l[5] = lBeforeRotation[7];
-        this.cube.l[6] = lBeforeRotation[0];
-        this.cube.l[7] = lBeforeRotation[3];
-        this.cube.l[8] = lBeforeRotation[6];
+        this.counterClockwiseRotation('l');
         return this;
     }
 
