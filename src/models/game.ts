6e70f9904e023eb35fdd56cf73ef979e0c41cf15
SimplyNate <horiuchi.nathan@gmail.com>
2023-10-29 13:47:43 -0700

    Flesh out CubeGame.step function to perform action and do rudimentary win or lose checks

--- src/models/game.ts
+++ src/models/game.ts
@@ -37,26 +37,93 @@ export function getRandomAction(): number {
 interface GameArgs {
     difficulty: number;
     gameType: 'randomLevels' | 'provided';
+    maxMoves?: number;
+}
+
+interface GameState {}
+
+interface Step {
+    reward: number;
+    state?: GameState;
+    done: boolean;
 }
 
 export class CubeGame {
+    maxMoves: number;
     cube: Cube;
     difficulty: number;
     gameType: string;
+    currentMove: number;
     constructor(args: GameArgs) {
         this.cube = new Cube();
         this.difficulty = args.difficulty;
         this.gameType = args.gameType;
+        if (args.maxMoves) {
+            this.maxMoves = args.maxMoves;
+        }
+        else {
+            this.maxMoves = Math.max(75 - this.difficulty, 25);
+        }
+        this.currentMove = 0;
     }
     reset() {
         this.initialize();
+        return this.getState();
     }
-    step() {
-        this.update();
+    step(action: number): Step {
+        const previousState = this.cube.toString();
+        if (action === ACTION_F) {
+            this.cube.f();
+        }
+        else if (action === ACTION_COUNTER_F) {
+            this.cube.counter_f();
+        }
+        if (action === ACTION_L) {
+            this.cube.l();
+        }
+        else if (action === ACTION_COUNTER_L) {
+            this.cube.counter_l();
+        }
+        if (action === ACTION_R) {
+            this.cube.r();
+        }
+        else if (action === ACTION_COUNTER_R) {
+            this.cube.counter_r();
+        }
+        if (action === ACTION_B) {
+            this.cube.b();
+        }
+        else if (action === ACTION_COUNTER_B) {
+            this.cube.counter_b();
+        }
+        if (action === ACTION_U) {
+            this.cube.u();
+        }
+        else if (action === ACTION_COUNTER_U) {
+            this.cube.counter_u();
+        }
+        if (action === ACTION_D) {
+            this.cube.d();
+        }
+        else if (action === ACTION_COUNTER_D) {
+            this.cube.counter_d();
+        }
+        let done = false;
+        // Check if cube is solved or if at max moves
+        if (this.cube.isSolved()) {
+            done = true;
+            return {reward: WIN_REWARD, done};
+        }
+        else if (this.currentMove === this.maxMoves) {
+            done = true;
+            return {reward: FAIL_REWARD, done};
+        }
+        const currentState = this.cube.toString();
+        let reward = UNSOLVED_REWARD;
+        // Check if current move solved a face
     }
-    private update() {}
     private initialize() {
-        this.cube = Cube.scrambled();
+        this.cube = Cube.scrambled(this.difficulty);
     }
     getState() {}
 }
