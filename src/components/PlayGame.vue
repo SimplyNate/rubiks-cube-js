b0ff8b97e78ab128470f7e24c1933b2e7e32dfc7
SimplyNate <horiuchi.nathan@gmail.com>
2023-11-17 21:22:16 -0800

    Assign all cubes a real color value

--- src/components/PlayGame.vue
+++ src/components/PlayGame.vue
@@ -8,7 +8,7 @@ const game = ref(new CubeGame);
 
 <template>
     <div style="display: flex;">
-        <div style="margin-right: 1em;">
+        <div style="margin-right: 1em; visibility: hidden;">
             <div>
                 <div class="cube"></div>
                 <div class="cube"></div>
@@ -27,6 +27,95 @@ const game = ref(new CubeGame);
         </div>
         <div style="margin-right: 1em;">
             <div>
+                <div :class="`cube ${game.cube.cube.u[0]}`"></div>
+                <div :class="`cube ${game.cube.cube.u[1]}`"></div>
+                <div :class="`cube ${game.cube.cube.u[2]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.u[3]}`"></div>
+                <div :class="`cube ${game.cube.cube.u[4]}`"></div>
+                <div :class="`cube ${game.cube.cube.u[5]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.u[6]}`"></div>
+                <div :class="`cube ${game.cube.cube.u[7]}`"></div>
+                <div :class="`cube ${game.cube.cube.u[8]}`"></div>
+            </div>
+        </div>
+    </div>
+    <div style="display: flex; margin-top: 0.5em; margin-bottom: 0.5em;">
+        <div style="margin-right: 1em;">
+            <div>
+                <div :class="`cube ${game.cube.cube.l[0]}`"></div>
+                <div :class="`cube ${game.cube.cube.l[1]}`"></div>
+                <div :class="`cube ${game.cube.cube.l[2]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.l[3]}`"></div>
+                <div :class="`cube ${game.cube.cube.l[4]}`"></div>
+                <div :class="`cube ${game.cube.cube.l[5]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.l[6]}`"></div>
+                <div :class="`cube ${game.cube.cube.l[7]}`"></div>
+                <div :class="`cube ${game.cube.cube.l[8]}`"></div>
+            </div>
+        </div>
+        <div style="margin-right: 1em;">
+            <div>
+                <div :class="`cube ${game.cube.cube.f[0]}`"></div>
+                <div :class="`cube ${game.cube.cube.f[1]}`"></div>
+                <div :class="`cube ${game.cube.cube.f[2]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.f[3]}`"></div>
+                <div :class="`cube ${game.cube.cube.f[4]}`"></div>
+                <div :class="`cube ${game.cube.cube.f[5]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.f[6]}`"></div>
+                <div :class="`cube ${game.cube.cube.f[7]}`"></div>
+                <div :class="`cube ${game.cube.cube.f[8]}`"></div>
+            </div>
+        </div>
+        <div style="margin-right: 1em;">
+            <div>
+                <div :class="`cube ${game.cube.cube.r[0]}`"></div>
+                <div :class="`cube ${game.cube.cube.r[1]}`"></div>
+                <div :class="`cube ${game.cube.cube.r[2]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.r[3]}`"></div>
+                <div :class="`cube ${game.cube.cube.r[4]}`"></div>
+                <div :class="`cube ${game.cube.cube.r[5]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.r[6]}`"></div>
+                <div :class="`cube ${game.cube.cube.r[7]}`"></div>
+                <div :class="`cube ${game.cube.cube.r[8]}`"></div>
+            </div>
+        </div>
+        <div style="margin-right: 1em;">
+            <div>
+                <div :class="`cube ${game.cube.cube.b[0]}`"></div>
+                <div :class="`cube ${game.cube.cube.b[1]}`"></div>
+                <div :class="`cube ${game.cube.cube.b[2]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.b[3]}`"></div>
+                <div :class="`cube ${game.cube.cube.b[4]}`"></div>
+                <div :class="`cube ${game.cube.cube.b[5]}`"></div>
+            </div>
+            <div>
+                <div :class="`cube ${game.cube.cube.b[6]}`"></div>
+                <div :class="`cube ${game.cube.cube.b[7]}`"></div>
+                <div :class="`cube ${game.cube.cube.b[8]}`"></div>
+            </div>
+        </div>
+    </div>
+    <div style="display: flex;">
+        <div style="margin-right: 1em; visibility: hidden;">
+            <div>
                 <div class="cube"></div>
                 <div class="cube"></div>
                 <div class="cube"></div>
@@ -44,70 +133,19 @@ const game = ref(new CubeGame);
         </div>
         <div style="margin-right: 1em;">
             <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
+                <div :class="`cube ${game.cube.cube.d[0]}`"></div>
+                <div :class="`cube ${game.cube.cube.d[1]}`"></div>
+                <div :class="`cube ${game.cube.cube.d[2]}`"></div>
             </div>
             <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
+                <div :class="`cube ${game.cube.cube.d[3]}`"></div>
+                <div :class="`cube ${game.cube.cube.d[4]}`"></div>
+                <div :class="`cube ${game.cube.cube.d[5]}`"></div>
             </div>
             <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
+                <div :class="`cube ${game.cube.cube.d[6]}`"></div>
+                <div :class="`cube ${game.cube.cube.d[7]}`"></div>
+                <div :class="`cube ${game.cube.cube.d[8]}`"></div>
             </div>
         </div>
     </div>
@@ -121,22 +159,22 @@ const game = ref(new CubeGame);
     border: 2px solid black;
     display: inline-block;
 }
-.white {
+.w {
     background-color: white;
 }
-.blue {
+.b {
     background-color: blue;
 }
-.orange {
+.o {
     background-color: orange;
 }
-.red {
+.r {
     background-color: red;
 }
-.green {
+.g {
     background-color: green;
 }
-.yellow {
+.y {
     background-color: yellow;
 }
 </style>
