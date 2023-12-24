389665f95a2eea548018682a59990d5689065d64
SimplyNate <horiuchi.nathan@gmail.com>
2023-12-24 13:16:48 -0800

    Add algorithms to solve

--- src/solve.ts
+++ src/solve.ts
@@ -1,6 +1,157 @@
 import { Cube } from './cube.js';
 
-const cube = new Cube();
+export function flip_edge(cube: Cube) {
+    cube
+        .f()
+        .counter_r()
+        .counter_d()
+        .r()
+        .f()
+        .f();
+    return cube;
+}
 
-cube.scramble(30);
+export function flip_adjacent_edges_rb(cube: Cube) {
+    cube
+        .counter_r()
+        .u()
+        .r()
+        .counter_u()
+        .counter_r();
+    return cube;
+}
+
+export function flip_opposite_edges_fb(cube: Cube) {
+    cube
+        .r()
+        .r()
+        .l()
+        .l()
+        .u()
+        .u()
+        .r()
+        .r()
+        .l()
+        .l();
+    return cube;
+}
+
+export function flip_corners_ftr_fbr(cube: Cube) {
+    cube
+        .counter_r()
+        .counter_d()
+        .r()
+        .d();
+    return cube;
+}
+
+export function move_edge_ft_fl(cube: Cube) {
+    cube
+        .counter_u()
+        .counter_l()
+        .u()
+        .l()
+        .u()
+        .f()
+        .counter_u()
+        .counter_f();
+    return cube;
+}
+
+export function move_edge_ft_fr(cube: Cube) {
+    cube
+        .u()
+        .r()
+        .counter_u()
+        .counter_r()
+        .counter_u()
+        .counter_f()
+        .u()
+        .f();
+    return cube;
+}
+
+export function permute_up_dot_to_l_shape(cube: Cube) {
+    cube
+        .f()
+        .r()
+        .u()
+        .counter_r()
+        .counter_u()
+        .counter_f();
+    return cube;
+}
+
+export function permute_up_dot_to_l_shape_fast(cube: Cube) {
+    cube
+        .f()
+        .u()
+        .r()
+        .counter_u()
+        .counter_r()
+        .counter_f();
+    return cube;
+}
+
+export function sune(cube: Cube) {
+    cube
+        .r()
+        .u()
+        .counter_r()
+        .u()
+        .r()
+        .u()
+        .u()
+        .counter_r();
+    return cube;
+}
+
+export function antisune(cube: Cube) {
+    cube
+        .u()
+        .u()
+        .r()
+        .u()
+        .u()
+        .counter_r()
+        .counter_u()
+        .r()
+        .counter_u()
+        .counter_r();
+    return cube;
+}
+
+export function permute_headlights(cube: Cube) {
+    cube
+        .counter_r()
+        .f()
+        .counter_r()
+        .b()
+        .b()
+        .r()
+        .counter_f()
+        .counter_r()
+        .b()
+        .b()
+        .r()
+        .r();
+    return cube;
+}
+
+export function permute_u(cube: Cube) {
+    cube
+        .r()
+        .counter_u()
+        .r()
+        .u()
+        .r()
+        .u()
+        .r()
+        .counter_u()
+        .counter_r()
+        .counter_u()
+        .r()
+        .r();
+    return cube;
+}
 
