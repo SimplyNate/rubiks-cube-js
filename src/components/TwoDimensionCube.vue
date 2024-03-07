311caac82416a2e220bfe9d7ce71604906c95c77
SimplyNate <horiuchi.nathan@gmail.com>
2024-03-06 19:29:31 -0800

    Implement dynamic visualization of solving the cube

--- src/components/TwoDimensionCube.vue
+++ src/components/TwoDimensionCube.vue
@@ -1,154 +1,234 @@
 <script setup lang="ts">
-import { type CubePositions } from '../cube.js';
+import {Color, Cube, Face} from '../cube.js';
+import {onMounted, ref} from "vue";
+import {solve} from "../solve.ts";
 
-defineProps<{
-    cube: CubePositions;
-}>();
+const cube = new Cube();
+
+const moveNumber = ref(0);
+
+function scramble() {
+    cube.scramble();
+    setColors();
+    clearMoveList();
+    moveNumber.value = 0;
+}
+
+function setColors() {
+    for (const face in cube.cube) {
+        for (let i = 0; i < cube.cube[face].length; i++) {
+            const color = cube.cube[face][i];
+            const elem = <Element>document.querySelector(`#${face}${i}`);
+
+            elem.setAttribute('class', `cube ${color}`);
+        }
+    }
+}
+
+function addMoveList(moveString: string) {
+    const elem = <Element>document.querySelector('#moveList');
+    const move = document.createElement('div');
+    move.innerText = moveString;
+    elem.appendChild(move);
+    const moveContainer = <Element>document.querySelector('#moveContainer');
+    moveContainer.scrollTo(0, moveContainer.scrollHeight);
+}
+
+function clearMoveList() {
+    const elem = <Element>document.querySelector('#moveList');
+    elem.innerHTML = '';
+}
+
+function solveCube() {
+    const targetCube = Cube.fromString(cube.toString());
+    solve(targetCube);
+    const interval = setInterval(() => {
+        const move = targetCube.history.shift();
+        if (move) {
+            moveNumber.value += 1;
+            addMoveList(move);
+            if (move.includes('reorient')) {
+                const reorientValue = move.split(' ')[1];
+                const [u, f] = reorientValue.split('');
+                cube.perform_reorientation(u as Color, f as Color);
+            }
+            else {
+                if (move.includes('counter')) {
+                    cube.performRotation(move.split('_')[1] as Face, true);
+                }
+                else {
+                    cube.performRotation(move as Face, false);
+                }
+            }
+            setColors();
+        }
+        else {
+            clearInterval(interval);
+        }
+    }, 50);
+}
+
+onMounted(() => {
+    setColors();
+});
 
 </script>
 
 <template>
     <div style="display: flex;">
-        <div style="margin-right: 1em; visibility: hidden;">
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
+        <div>
+            <div style="display: flex;">
+                <div style="margin-right: 1em; visibility: hidden;">
+                    <div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                    </div>
+                    <div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                    </div>
+                    <div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                    </div>
+                </div>
+                <div style="margin-right: 1em;">
+                    <div>
+                        <div class="cube" id="u0"></div>
+                        <div class="cube" id="u1"></div>
+                        <div class="cube" id="u2"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="u3"></div>
+                        <div class="cube" id="u4"></div>
+                        <div class="cube" id="u5"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="u6"></div>
+                        <div class="cube" id="u7"></div>
+                        <div class="cube" id="u8"></div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
+            <div style="display: flex; margin-top: 0.5em; margin-bottom: 0.5em;">
+                <div style="margin-right: 1em;">
+                    <div>
+                        <div class="cube" id="l0"></div>
+                        <div class="cube" id="l1"></div>
+                        <div class="cube" id="l2"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="l3"></div>
+                        <div class="cube" id="l4"></div>
+                        <div class="cube" id="l5"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="l6"></div>
+                        <div class="cube" id="l7"></div>
+                        <div class="cube" id="l8"></div>
+                    </div>
+                </div>
+                <div style="margin-right: 1em;">
+                    <div>
+                        <div class="cube" id="f0"></div>
+                        <div class="cube" id="f1"></div>
+                        <div class="cube" id="f2"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="f3"></div>
+                        <div class="cube" id="f4"></div>
+                        <div class="cube" id="f5"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="f6"></div>
+                        <div class="cube" id="f7"></div>
+                        <div class="cube" id="f8"></div>
+                    </div>
+                </div>
+                <div style="margin-right: 1em;">
+                    <div>
+                        <div class="cube" id="r0"></div>
+                        <div class="cube" id="r1"></div>
+                        <div class="cube" id="r2"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="r3"></div>
+                        <div class="cube" id="r4"></div>
+                        <div class="cube" id="r5"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="r6"></div>
+                        <div class="cube" id="r7"></div>
+                        <div class="cube" id="r8"></div>
+                    </div>
+                </div>
+                <div style="margin-right: 1em;">
+                    <div>
+                        <div class="cube" id="b0"></div>
+                        <div class="cube" id="b1"></div>
+                        <div class="cube" id="b2"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="b3"></div>
+                        <div class="cube" id="b4"></div>
+                        <div class="cube" id="b5"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="b6"></div>
+                        <div class="cube" id="b7"></div>
+                        <div class="cube" id="b8"></div>
+                    </div>
+                </div>
             </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
+            <div style="display: flex;">
+                <div style="margin-right: 1em; visibility: hidden;">
+                    <div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                    </div>
+                    <div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                    </div>
+                    <div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                        <div class="cube"></div>
+                    </div>
+                </div>
+                <div style="margin-right: 1em;">
+                    <div>
+                        <div class="cube" id="d0"></div>
+                        <div class="cube" id="d1"></div>
+                        <div class="cube" id="d2"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="d3"></div>
+                        <div class="cube" id="d4"></div>
+                        <div class="cube" id="d5"></div>
+                    </div>
+                    <div>
+                        <div class="cube" id="d6"></div>
+                        <div class="cube" id="d7"></div>
+                        <div class="cube" id="d8"></div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div :class="`cube ${cube.u[0]}`"></div>
-                <div :class="`cube ${cube.u[1]}`"></div>
-                <div :class="`cube ${cube.u[2]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.u[3]}`"></div>
-                <div :class="`cube ${cube.u[4]}`"></div>
-                <div :class="`cube ${cube.u[5]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.u[6]}`"></div>
-                <div :class="`cube ${cube.u[7]}`"></div>
-                <div :class="`cube ${cube.u[8]}`"></div>
-            </div>
+        <div style="margin-left: 50px; height: 400px; width: 100px; overflow: auto;" id="moveContainer">
+            <div>Move List</div>
+            <div id="moveList"></div>
         </div>
+        <div>Moves to solve: <span id="moveNumberTracker">{{ moveNumber }}</span></div>
     </div>
-    <div style="display: flex; margin-top: 0.5em; margin-bottom: 0.5em;">
-        <div style="margin-right: 1em;">
-            <div>
-                <div :class="`cube ${cube.l[0]}`"></div>
-                <div :class="`cube ${cube.l[1]}`"></div>
-                <div :class="`cube ${cube.l[2]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.l[3]}`"></div>
-                <div :class="`cube ${cube.l[4]}`"></div>
-                <div :class="`cube ${cube.l[5]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.l[6]}`"></div>
-                <div :class="`cube ${cube.l[7]}`"></div>
-                <div :class="`cube ${cube.l[8]}`"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div :class="`cube ${cube.f[0]}`"></div>
-                <div :class="`cube ${cube.f[1]}`"></div>
-                <div :class="`cube ${cube.f[2]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.f[3]}`"></div>
-                <div :class="`cube ${cube.f[4]}`"></div>
-                <div :class="`cube ${cube.f[5]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.f[6]}`"></div>
-                <div :class="`cube ${cube.f[7]}`"></div>
-                <div :class="`cube ${cube.f[8]}`"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div :class="`cube ${cube.r[0]}`"></div>
-                <div :class="`cube ${cube.r[1]}`"></div>
-                <div :class="`cube ${cube.r[2]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.r[3]}`"></div>
-                <div :class="`cube ${cube.r[4]}`"></div>
-                <div :class="`cube ${cube.r[5]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.r[6]}`"></div>
-                <div :class="`cube ${cube.r[7]}`"></div>
-                <div :class="`cube ${cube.r[8]}`"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div :class="`cube ${cube.b[0]}`"></div>
-                <div :class="`cube ${cube.b[1]}`"></div>
-                <div :class="`cube ${cube.b[2]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.b[3]}`"></div>
-                <div :class="`cube ${cube.b[4]}`"></div>
-                <div :class="`cube ${cube.b[5]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.b[6]}`"></div>
-                <div :class="`cube ${cube.b[7]}`"></div>
-                <div :class="`cube ${cube.b[8]}`"></div>
-            </div>
-        </div>
-    </div>
-    <div style="display: flex;">
-        <div style="margin-right: 1em; visibility: hidden;">
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-            <div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-                <div class="cube"></div>
-            </div>
-        </div>
-        <div style="margin-right: 1em;">
-            <div>
-                <div :class="`cube ${cube.d[0]}`"></div>
-                <div :class="`cube ${cube.d[1]}`"></div>
-                <div :class="`cube ${cube.d[2]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.d[3]}`"></div>
-                <div :class="`cube ${cube.d[4]}`"></div>
-                <div :class="`cube ${cube.d[5]}`"></div>
-            </div>
-            <div>
-                <div :class="`cube ${cube.d[6]}`"></div>
-                <div :class="`cube ${cube.d[7]}`"></div>
-                <div :class="`cube ${cube.d[8]}`"></div>
-            </div>
-        </div>
+    <div>
+        <button @click="scramble">Scramble</button>
+        <button @click="solveCube">Solve</button>
     </div>
 </template>
 
